b0bde2ef34483b8bb4770cad28a10ab6ace771e3
Noelg14 <griffin.noel1@gmail.com>
2021-05-07 08:50:10 +0100

    Added todo

--- src/oopTest.java
+++ src/oopTest.java
@@ -6,6 +6,14 @@ public class oopTest
     {
       transaction.guiT(); //need to add validation to mask sure there are no blanks, as well as an option pane to create person.
       //person.guiP();
+
+      /*
+        TODO: Add login, which validates vs DB entries
+        TODO: Add Screen to select Between guiT and GuiP 
+        TODO: add get/set methods for person/ transaction
+        TODO: Get a 'login' associated with transactions
+        TODO: Tidy up GUIs, copy/paste doesnt look pretty 
+      */
     }
 }
 
